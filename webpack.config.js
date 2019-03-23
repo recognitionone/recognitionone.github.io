a759c70e53a2b3a3eadc5052e5d81698c2f574a6
recognitionone <recognitionone@gmail.com>
2019-03-23 23:00:09 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+	plugins: [new HtmlWebpackPlugin()],
+	module: {
+	rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+					presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader'],
+			}
+		]
+	}
+};
