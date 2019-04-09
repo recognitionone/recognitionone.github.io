f4023c6ec3e3cf5ca1d91c3b90bcd351734a1c89
recognitionone <recognitionone@gmail.com>
2019-04-09 16:12:44 +0200

    Changed 24 hour day to 12 hour day

--- excercise-3-clock-generator/src/script.js
+++ excercise-3-clock-generator/src/script.js
@@ -7,58 +7,58 @@ export default function() {
   div.appendChild(h1);
   body.appendChild(div);
 
-var date = new Date();
+  var date = new Date();
 
-var s = date.getSeconds();
-var m = date.getMinutes();
-var h = date.getHours();
+  var s = date.getSeconds();
+  var m = date.getMinutes();
+  var h = date.getHours()%12;
 
-function *uniqSec() {
-  while (true) {
-    (s <= 59) ? (yield s, s++) : (s = 0, yield s, s++)
+  function *uniqSec() {
+    while (true) {
+      (s <= 59) ? (yield s, s++) : (s = 0, yield s, s++)
+    }
   }
-}
 
-function *uniqMin() {
-  while(true) {
-    (m < 59) ? ( (s < 59) ? yield m : (m++, yield m) ) : (m = 0, yield m)
+  function *uniqMin() {
+    while(true) {
+      (m < 59) ? ( (s < 59) ? yield m : (m++, yield m) ) : (m = 0, yield m)
+    }
   }
-}
 
-function *uniqHour() {
-  while(true) {
-    (h < 24) ? ( (m < 59) ? yield h : (h++, yield h) ) : (h = 0, yield h)
+  function *uniqHour() {
+    while(true) {
+      (h < 12) ? ( (m < 59) ? yield h : (h++, yield h) ) : (h = 0, yield h)
+    }
   }
-}
 
-var sec = uniqSec();
-var min = uniqMin();
-var hour = uniqHour();
+  var sec = uniqSec();
+  var min = uniqMin();
+  var hour = uniqHour();
 
-var showTime = function(){
+  var showTime = function(){
     var outputFormat = "hh:mm:ss";
     setFormatFromInput();
     function setFormatFromInput() {
       h1.innerHTML = formatDate(outputFormat);
     }
-};
-
-function formatDate(format){
-  let h = hour.next().value;
-  let m = min.next().value;
-  let s = sec.next().value;
-
-  function ii(i, len) { let s = i + ""; len = len || 2; while (s.length < len) s = "0" + s; return s; }
-
-  format = format.replace(/(^|[^\\])hh+/g, "$1" + ii(h));
-  format = format.replace(/(^|[^\\])mm+/g, "$1" + ii(m));
-  format = format.replace(/(^|[^\\])ss+/g, "$1" + ii(s));
-  
-  return format;
   };
 
-onload = setInterval(function() {
-  showTime();
-}, 1000);
+  function formatDate(format){
+    let h = hour.next().value;
+    let m = min.next().value;
+    let s = sec.next().value;
+
+    function ii(i, len) { let s = i + ""; len = len || 2; while (s.length < len) s = "0" + s; return s; }
+
+    format = format.replace(/(^|[^\\])hh+/g, "$1" + ii(h));
+    format = format.replace(/(^|[^\\])mm+/g, "$1" + ii(m));
+    format = format.replace(/(^|[^\\])ss+/g, "$1" + ii(s));
+    
+    return format;
+  };
+
+  onload = setInterval(function() {
+    showTime();
+  }, 1000);
 
 }
